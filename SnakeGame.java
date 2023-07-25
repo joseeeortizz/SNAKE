c05f9b728f61f081a76fe68ba4947ea02e36b4d8
Jose Ortiz <137450801+joseeeortizz@users.noreply.github.com>
2023-07-25 19:28:17 -0400

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,219 @@
+package com.ortizgame;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class SnakeGame extends JFrame {
+
+    public SnakeGame() {
+        add(new Board());
+        setResizable(false);
+        pack();
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new SnakeGame().setVisible(true);
+    }
+}
+
+class Board extends JPanel implements ActionListener {
+
+    private final int WIDTH = 300;
+    private final int HEIGHT = 300;
+    private final int DOT_SIZE = 10;
+    private final int ALL_DOTS = 900;
+    private final int RAND_POS = 29;
+    private final int DELAY = 140;
+
+    private int[] x = new int[ALL_DOTS];
+    private int[] y = new int[ALL_DOTS];
+
+    private int dots;
+    private int apple_x;
+    private int apple_y;
+
+    private boolean left = false;
+    private boolean right = true;
+    private boolean up = false;
+    private boolean down = false;
+    private boolean inGame = true;
+
+    private Timer timer;
+    private Random rand;
+
+    public Board() {
+        setBackground(Color.BLACK);
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setFocusable(true);
+        addKeyListener(new TAdapter());
+
+        initGame();
+    }
+
+    private void initGame() {
+        dots = 3;
+
+        for (int i = 0; i < dots; i++) {
+            x[i] = 50 - i * DOT_SIZE;
+            y[i] = 50;
+        }
+
+        rand = new Random();
+        newApple();
+
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    private void newApple() {
+        apple_x = rand.nextInt(RAND_POS) * DOT_SIZE;
+        apple_y = rand.nextInt(RAND_POS) * DOT_SIZE;
+    }
+
+    private void move() {
+        for (int i = dots; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+
+        if (left) {
+            x[0] -= DOT_SIZE;
+        }
+
+        if (right) {
+            x[0] += DOT_SIZE;
+        }
+
+        if (up) {
+            y[0] -= DOT_SIZE;
+        }
+
+        if (down) {
+            y[0] += DOT_SIZE;
+        }
+    }
+
+    private void checkApple() {
+        if (x[0] == apple_x && y[0] == apple_y) {
+            dots++;
+            newApple();
+        }
+    }
+
+    private void checkCollision() {
+        for (int i = dots; i > 0; i--) {
+            if (i > 4 && x[0] == x[i] && y[0] == y[i]) {
+                inGame = false;
+            }
+        }
+
+        if (y[0] >= HEIGHT) {
+            inGame = false;
+        }
+
+        if (y[0] < 0) {
+            inGame = false;
+        }
+
+        if (x[0] >= WIDTH) {
+            inGame = false;
+        }
+
+        if (x[0] < 0) {
+            inGame = false;
+        }
+
+        if (!inGame) {
+            timer.stop();
+        }
+    }
+
+    private void gameOver(Graphics g) {
+        String msg = "Game Over";
+        java.awt.Font small = new java.awt.Font("Helvetica", java.awt.Font.BOLD, 14);
+        java.awt.FontMetrics metr = getFontMetrics(small);
+
+        g.setColor(Color.WHITE);
+        g.setFont(small);
+        g.drawString(msg, (WIDTH - metr.stringWidth(msg)) / 2, HEIGHT / 2);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (inGame) {
+            g.setColor(Color.RED);
+            g.fillOval(apple_x, apple_y, DOT_SIZE, DOT_SIZE);
+
+            for (int i = 0; i < dots; i++) {
+                if (i == 0) {
+                    g.setColor(Color.GREEN);
+                } else {
+                    g.setColor(new Color(45, 180, 0));
+                }
+                g.fillRect(x[i], y[i], DOT_SIZE, DOT_SIZE);
+            }
+
+            java.awt.Toolkit.getDefaultToolkit().sync();
+
+        } else {
+            gameOver(g);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (inGame) {
+            checkApple();
+            checkCollision();
+            move();
+        }
+
+        repaint();
+    }
+
+    private class TAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+
+            if ((key == KeyEvent.VK_LEFT) && (!right)) {
+                left = true;
+                up = false;
+                down = false;
+            }
+
+            if ((key == KeyEvent.VK_RIGHT) && (!left)) {
+                right = true;
+                up = false;
+                down = false;
+            }
+
+            if ((key == KeyEvent.VK_UP) && (!down)) {
+                up = true;
+                right = false;
+                left = false;
+            }
+
+            if ((key == KeyEvent.VK_DOWN) && (!up)) {
+                down = true;
+                right = false;
+                left = false;
+            }
+        }
+    }
+}
